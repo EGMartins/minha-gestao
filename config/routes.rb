7888251b4696f35f7ac53c6885cd0cce238be57d
Eduardo Martins <mirassol.code@gmail.com>
2018-03-04 01:51:01 -0300

    Creating a simple home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
   resources :recipes
   resources :expenses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
